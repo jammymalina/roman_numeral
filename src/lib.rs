0a05d382ac346f55efd6544921724d1a8df17e0b
jammymalina <jakub.jantosik@gmail.com>
2020-04-02 00:38:45 +0200

    Added a few functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,65 @@
 
-const ROMAN_NUMERAL_SYMBOLS: &str = "IVXLCDM";
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum RomanNumeralError {
     InvalidStr,
     NumberOutOfRange,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RomanNumeral {
-    value: String,
+    value: u32,
 }
 
 impl RomanNumeral {
-    pub fn is_valid_str(value: &str) -> bool {
-        true
+    fn get_roman_symbols() -> Vec<char> {
+        let roman_symbols = "MDCLXVI";
+        roman_symbols.chars().collect()
+    }
+
+    fn get_symbol_value(symbol: char) -> Result<u32, RomanNumeralError> {
+        let roman_symbols = Self::get_roman_symbols();
+        let index = roman_symbols.into_iter().rev().position(|s| s == symbol);
+        match index {
+            Some(i) => Ok(5u32.pow(i as u32 % 2) * 10u32.pow(i as u32 / 2)),
+            None => Err(RomanNumeralError::InvalidStr),
+        }
     }
 
     pub fn max_value() -> u32 {
-        4999
+        let n = Self::get_roman_symbols().len() as u32;
+        4u32.pow(n % 2) * 10u32.pow(n / 2) - 1
     }
 
     pub fn get(&self) -> String {
-        self.value.clone()
-    }
-
-    pub fn get_u32() -> u32 {
-        0
-    }
-
-    pub fn from_string(value: &str) -> Result<RomanNumeral, RomanNumeralError> {
-        if !Self::is_valid_str(value) {
-            return Err(RomanNumeralError::InvalidStr);
+        let roman_symbols = Self::get_roman_symbols();
+        let mut roman_numeral = vec![];
+        let mut num = self.value;
+        let mut symbol_index = roman_symbols.len() - 1;
+        while num > 0 {
+            let cipher = (num % 10) as usize;
+            let rom_sym_1 = roman_symbols[symbol_index];
+            let rom_sym_5 = roman_symbols[symbol_index.saturating_sub(1)];
+            let rom_sym_10 = roman_symbols[symbol_index.saturating_sub(2)];
+            let roman_symbol = match cipher {
+                1..=3 => rom_sym_1.to_string().repeat(cipher),
+                4 => vec![rom_sym_1, rom_sym_5].into_iter().collect(),
+                5 => rom_sym_5.to_string(),
+                6..=8 => std::iter::once(rom_sym_5)
+                    .chain(std::iter::repeat(rom_sym_1).take(cipher - 5))
+                    .collect(),
+                9 => vec![rom_sym_1, rom_sym_10].into_iter().collect(),
+                _ => String::from(""),
+            };
+            roman_numeral.push(roman_symbol);
+            symbol_index = symbol_index.saturating_sub(2);
+            num /= 10;
         }
 
-        Ok(RomanNumeral {
-            value: String::from(value),
-        })
+        roman_numeral.into_iter().rev().collect::<Vec<String>>().join("")
+    }
+
+    pub fn get_u32(&self) -> u32 {
+        self.value
     }
 
     pub fn from_u32(value: u32) -> Result<RomanNumeral, RomanNumeralError> {
@@ -45,7 +68,35 @@ impl RomanNumeral {
         }
 
         Ok(RomanNumeral {
-            value: String::from(""),
+            value,
         })
     }
+
+    pub fn from_string(value: &str) -> Result<RomanNumeral, RomanNumeralError> {
+        let mut total = 0;
+        let mut minus = 0;
+        let symbols: Vec<_> = value.chars().collect();
+        for i in 0..symbols.len() {
+            let num = Self::get_symbol_value(symbols[i])?;
+            if i == value.len() - 1 ||
+                num >= Self::get_symbol_value(symbols[i + 1])?
+            {
+                total += num - minus;
+                minus = 0;
+                continue;
+            }
+            minus = num;
+        }
+
+        let result = RomanNumeral::from_u32(total);
+        if result.is_err() {
+            return Err(RomanNumeralError::InvalidStr);
+        }
+        let s = result.clone().unwrap().get();
+        if s != value {
+            return Err(RomanNumeralError::InvalidStr);
+        }
+        result
+    }
+
 }
--- tests/roman_numeral.rs
+++ tests/roman_numeral.rs
@@ -1,10 +1,43 @@
 #[cfg(test)]
 mod tests {
     use roman_numeral::RomanNumeral;
-    
+
+    #[test]
+    fn it_should_return_max_value() {
+        let max_value = RomanNumeral::max_value();
+        assert_eq!(max_value, 3999);
+    }
+
+    #[test]
+    fn it_should_convert_from_basic_u32() {
+        let roman = RomanNumeral::from_u32(1).unwrap();
+        assert_eq!(roman.get(), String::from("I"));
+
+        let roman = RomanNumeral::from_u32(4).unwrap();
+        assert_eq!(roman.get(), String::from("IV"));
+
+        let roman = RomanNumeral::from_u32(5).unwrap();
+        assert_eq!(roman.get(), String::from("V"));
+
+        let roman = RomanNumeral::from_u32(8).unwrap();
+        assert_eq!(roman.get(), String::from("VIII"));
+
+        let roman = RomanNumeral::from_u32(9).unwrap();
+        assert_eq!(roman.get(), String::from("IX"));
+
+        let roman = RomanNumeral::from_u32(10).unwrap();
+        assert_eq!(roman.get(), String::from("X"));
+    }
+
     #[test]
     fn it_should_convert_from_u32() {
-        let roman_1 = RomanNumeral::from_u32(1).unwrap();
-        assert_eq!(roman_1.get(), String::from("I"));
+        let roman = RomanNumeral::from_u32(49).unwrap();
+        assert_eq!(roman.get(), String::from("XLIX"));
+
+        let roman = RomanNumeral::from_u32(3999).unwrap();
+        assert_eq!(roman.get(), String::from("MMMCMXCIX"));
+
+        let roman = RomanNumeral::from_u32(86).unwrap();
+        assert_eq!(roman.get(), String::from("LXXXVI"));
     }
 }
