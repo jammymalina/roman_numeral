4e906b7e6b484311d24bc8da6bd09f3a981338f6
jammymalina <jakub.jantosik@gmail.com>
2020-04-02 09:58:18 +0200

    Updated tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum RomanNumeralError {
     InvalidStr,
     NumberOutOfRange,
--- tests/roman_numeral.rs
+++ tests/roman_numeral.rs
@@ -1,6 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use roman_numeral::RomanNumeral;
+    use roman_numeral::{RomanNumeral, RomanNumeralError};
+
+    fn test_numeral_error(numeral: Result<RomanNumeral, RomanNumeralError>,
+        expected_error: RomanNumeralError)
+    {
+        match numeral {
+            Err(e) => assert_eq!(e, expected_error),
+            _ => panic!("It should have thrown an error"),
+        };
+    }
 
     #[test]
     fn it_should_return_max_value() {
@@ -9,7 +18,34 @@ mod tests {
     }
 
     #[test]
-    fn it_should_convert_from_basic_u32() {
+    fn it_should_create_from_roman_numeral_string() {
+        let roman = RomanNumeral::from_string("I").unwrap();
+        assert_eq!(roman.get_u32(), 1);
+
+        let roman = RomanNumeral::from_string("III").unwrap();
+        assert_eq!(roman.get_u32(), 3);
+
+        let roman = RomanNumeral::from_string("X").unwrap();
+        assert_eq!(roman.get_u32(), 10);
+
+        let roman = RomanNumeral::from_string("CXXV").unwrap();
+        assert_eq!(roman.get_u32(), 125);
+    }
+
+    #[test]
+    fn it_should_not_create_from_roman_numeral_string() {
+        let roman = RomanNumeral::from_string("IIII");
+        test_numeral_error(roman, RomanNumeralError::InvalidStr);
+
+        let roman = RomanNumeral::from_string("MMIIII");
+        test_numeral_error(roman, RomanNumeralError::InvalidStr);
+
+        let roman = RomanNumeral::from_string("MMXXXXIV");
+        test_numeral_error(roman, RomanNumeralError::InvalidStr);
+    }
+
+    #[test]
+    fn it_should_init_convert_basic_u32() {
         let roman = RomanNumeral::from_u32(1).unwrap();
         assert_eq!(roman.get(), String::from("I"));
 
@@ -39,5 +75,14 @@ mod tests {
 
         let roman = RomanNumeral::from_u32(86).unwrap();
         assert_eq!(roman.get(), String::from("LXXXVI"));
+    }
+
+    #[test]
+    fn it_should_not_init_fromu32() {
+        let roman = RomanNumeral::from_u32(0);
+        test_numeral_error(roman, RomanNumeralError::NumberOutOfRange);
+
+        let roman = RomanNumeral::from_u32(4000);
+        test_numeral_error(roman, RomanNumeralError::NumberOutOfRange);
     }
 }
